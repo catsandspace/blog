88f7cfd1d673fda303aa581e0a2ee75c76f40cd2
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-08 12:03:15 +0100

    initial commit

---
+++ assets/config-example.php
@@ -0,0 +1,8 @@
+<?php
+  // Variables used to connect to the database.
+  $db_hostname = ""; // The hostname of the database.
+  $db_username = ""; // The username of the database.
+  $db_password = ""; // The password of the database.
+  $db_name = ""; // The name of the database.
+
+?>
