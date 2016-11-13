bf8258a8aff474c494359610ced2e1c18f1a5594
xhkr-pc <hugo.kreipke@gmail.com>
2016-11-13 20:20:34 +0100

    Added header w. nav + categories db-connection

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,13 @@
+<?php
+	// Execute categories query.
+	$query = "SELECT * FROM categories";
+	if ($stmt->prepare($query)) {
+		$stmt->execute();
+		$stmt->bind_result($id, $category);
+	}
+
+?>
+
 <!DOCTYPE html>
 <html lang="sv">
 <head>
@@ -7,3 +17,16 @@
   <title>Cats and Space</title>
 </head>
 <body>
+<header>
+	<img src="img/logo.png" alt="logo">
+	<h1 class="logo-title">Cats and Space</h1>
+	<nav>
+		<ul>
+			<li><a href="index.php">Hem</a></li>
+			<?php while (mysqli_stmt_fetch($stmt)): ?>
+			<li><a href="?display=<?php echo $category; ?>"><?php echo ucfirst($category); ?></a></li>
+			<?php endwhile?>
+			<li><a href="#">Kontakt</a></li>
+		</ul>
+	</nav>
+</header>
