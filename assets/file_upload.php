ed0dd56d0bba1f346b64bceec0b23f31a6f41449
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-14 14:21:42 +0100

    Changed all variable names (from underscore to camelCase)

--- assets/file_upload.php
+++ assets/file_upload.php
@@ -4,17 +4,17 @@ require_once(__DIR__."/functions.php");
 // Check if user has pressed the submit button on addpost.php.
 if (isset($_POST["submit"])) {
 
-    $target_folder = "../uploads/postimg/"; // The folder in which the file should be saved.
+    $targetFolder = "../uploads/postimg/"; // The folder in which the file should be saved.
 
-    $file_name = basename($_FILES["post-img"]["name"]); //The name of the file
-    $temporary_file = $_FILES["post-img"]["tmp_name"]; // The temporary file and path
-    $type = pathinfo($file_name, PATHINFO_EXTENSION); // The file type
-    $file_error = checkUploadedFile($_FILES["post-img"]); // This checks if there are any file errors
+    $fileName = basename($_FILES["post-img"]["name"]); //The name of the file
+    $temporaryFile = $_FILES["post-img"]["tmp_name"]; // The temporary file and path
+    $type = pathinfo($fileName, PATHINFO_EXTENSION); // The file type
+    $fileError = checkUploadedFile($_FILES["post-img"]); // This checks if there are any file errors
 
-    $target_name = $target_folder . basename("postimg") . ".$type";
+    $targetName = $targetFolder . basename("postimg") . ".$type";
 
     // Move file to "../uploads/postimg/"
-    if (!$file_error && move_uploaded_file($temporary_file, $target_name)) {
+    if (!$fileError && move_uploaded_file($temporaryFile, $targetName)) {
         echo "File upload success.";
     }
 }
