232dd64de7eb794df0afda930fdf47d1c12505b8
xhkr.mac <hkrei@hodor.lan>
2016-11-14 20:54:21 +0100

    Removed require_once parentheses

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once(__DIR__."/config.php");
+    require_once __DIR__."/config.php";
 
     $conn = new mysqli($db_hostname, $db_user, $db_password, $db_name);
     $conn->set_charset("utf8");
@@ -8,5 +8,5 @@
     $databaseErrorMessage = "Det går inte att ansluta till databasen just nu (felkod: $databaseError).<br>Försök igen senare.";
 
     $stmt = $conn->stmt_init();
-
+    
 ?>
