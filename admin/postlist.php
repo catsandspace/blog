40ff6ba2335ccdde1e7af8b7dfb0826fe035854e
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-15 20:08:23 +0100

    Added feedback message
    Edited:
    **admin/postlist.php** <- Added feedback message if user clicks on
    buttons for editing or deleting posts.
    
    **uploads/postimg/** <- Added a placeholder image for printing out
    image url in post list.

--- admin/postlist.php
+++ admin/postlist.php
@@ -2,6 +2,7 @@
     require_once "../templates/header.php";
     require_once "../assets/session.php";
 
+    // This redirects user to login.php if not logged in.
     if (!isset($_SESSION["logged-in"]) && $_SESSION["logged-in"] == false) {
         header("Location: ../login.php");
     }
@@ -15,14 +16,45 @@
        $stmt->execute();
        $stmt->bind_result($id, $userId, $created, $updated, $image, $title, $content, $published, $categoryId, $categoryName);
    }
+
+/* -----------------------------------------------------------------------------
+   START OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
+----------------------------------------------------------------------------- */
+
+    $feedbackMessage = "";
+
+    if (isset($_POST["edit-post"])) {
+        $feedbackMessage = "Du vill redigera ett inlägg";
+    }
+
+    if (isset($_POST["delete-post"])) {
+        $feedbackMessage = "Du vill ta bort ett inlägg";
+    }
+
+/* -----------------------------------------------------------------------------
+   END OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
+----------------------------------------------------------------------------- */
 ?>
 <h2>Inlägg</h2>
-<table>
-<?php while (mysqli_stmt_fetch($stmt)): ?>
-    <tr>
-        <td><?php echo $image; ?></td>
-        <td><?php echo $title; ?></td>
-    </tr>
-<?php endwhile; ?>
-</table>
+<?php if ($feedbackMessage) { echo $feedbackMessage; } ?>
+<form method="POST" action="./postlist.php">
+    <table>
+        <thead>
+            <td>Foto</td>
+            <td>Rubrik</td>
+            <td>Redigera</td>
+            <td>Ta bort</td>
+        </thead>
+        </tbody>
+            <?php while (mysqli_stmt_fetch($stmt)): ?>
+            <tr>
+                <td><img src="../uploads/postimg/postlist-img.jpg" alt="Image of cats and space" class="postlist-img"></td>
+                <td><h3><?php echo $title; ?></h3></td>
+                <td><button type="submit" class="button" name="edit-post" value="<?php echo $id; ?>">Redigera</button></td>
+                <td><button type="submit" class="button" name="delete-post" value="<?php echo $id; ?>">Ta bort  </button></td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</form>
 <?php require_once "../templates/footer.php"; ?>
