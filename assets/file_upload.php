57b7c1256f7119516b28cc40e95826959534850f
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-15 19:37:08 +0100

    Fix: corrected URL which is sent to database
    Edited:
    **admin/addpost.php** <- changed variable $image, which is sent to the
    database. Before, the path to the file in a temporary folder was added.
    Now the path is updated to _uploads/postimg/_.
    
    **assets/file_upload.php** <- Added a TODO, since the target name
    differs from the URL used on index.php.

--- assets/file_upload.php
+++ assets/file_upload.php
@@ -11,11 +11,12 @@ if (isset($_POST["submit"])) {
     $type = pathinfo($fileName, PATHINFO_EXTENSION); // The file type
     $fileError = checkUploadedFile($_FILES["post-img"]); // This checks if there are any file errors
 
+    // TODO: This URL differs from the URL's used on index.php. Don't forget to sort this out.
     $targetName = $targetFolder . basename("postimg") . ".$type";
 
     // Move file to "../uploads/postimg/"
-    if (!$fileError && move_uploaded_file($temporaryFile, $targetName)) {
-        echo "File upload success.";
+    if (!$fileError) {
+        move_uploaded_file($temporaryFile, $targetName);
     }
 }
 ?>
