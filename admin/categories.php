593d4109171f7a6be9e08c0d6030ef76a892e868
aalselius <alselius@telia.com>
2016-11-15 12:34:16 +0100

    Updated catagories, users

--- admin/categories.php
+++ admin/categories.php
@@ -1,81 +1,112 @@
 <?php
-    require_once (__DIR__."/../assets/db_connect.php");
+	include_once "../assets/db_connect.php"; // Database connection.
+    include_once "../templates/header.php"; // Header content.
 
-    if (isset ($_GET["addCat"])) {
-        $category = strip_tags($_GET["category"]);
-        $query = "INSERT INTO categories VALUES (NULL, '$category')";
-        if ($stmt -> prepare($query)) {
-            $stmt->execute();
-        } else {
-            echo "fel";
-        }
-    }
+    // Reset functions for the internal variables
+    $changeCategoryId = NULL;
+    $errorMessage = NULL;
 
-    if (isset ($_GET["removeCat"])) {
-        if (!empty($_GET["checkList"])) {
-            foreach ($_GET['checkList'] as $selected) {
+    // Select all rows from the database categories 
+    $query = "SELECT * FROM categories";
+    if ($stmt -> prepare($query)):
+        $stmt-> execute();
+        $stmt -> bind_result($catId, $cat);
+    endif;
+
+    // If-statement to check if button for adding new categories is set 
+    // If everything looks okay insert into db
+    if (isset ($_GET["addCat"])):
+        if (!empty($_GET["category"])): // Input given
+            $category = mysql_real_escape_string($_GET["category"]);
+            $query = "INSERT INTO categories VALUES (NULL, '$category')";
+            if ($stmt -> prepare($query)):
+                $stmt->execute();
+            else:
+                $errorMessage ="Faulty query in addCat";
+            endif;
+        else:
+            $errorMessage ="Du måste ange en kategori"; // No input given
+    endif;
+
+    // If-statement to check if button for removing categories is set 
+    // If button is pressed continue to check through the array  and
+    // for each category checked, remove it frpm the db
+    if (isset ($_GET["removeCat"])):
+        if (!empty($_GET["checkList"])):
+            foreach ($_GET['checkList'] as $selected):
                 $catId = $selected;
                 $query = "DELETE FROM categories WHERE id=$catId";
-                if ($stmt -> prepare($query)) {
+                if ($stmt -> prepare($query)):
                     $stmt->execute();
-                } else {
-                    echo "fel";
-                } //end if
-            } // end foreach              
-        } else {
-            echo "fellist";
-        }
-    }
+                else:
+                    $errorMessage ="Faulty query in removeCat";
+                endif;
+            endforeach;              
+        else:
+            $errorMessage ="Ange kategori att radera";
+        endif;
+    endif;
             
+    // If-statement to check if button for changing categories is set 
+    // A counter is set to see if only one category is checked
+    // The id for the checked category is memorized, if nothing is 
+    // checked the category id is set to NULL
+    if (isset ($_GET["changeCat"])):
+        if (!empty($_GET["checkList"])):
+            $count=0;
+            foreach ($_GET['checkList'] as $selected):
+                $catId = $selected;
+                $count ++;
+            endforeach;
+            if ($count > 1):
+                $errorMessage ="Du kan bara välja en.";
+                $catId = NULL;
+            else:
+                $changeCategoryId = $selected;
+            endif;
+        endif; 
+    endif;
 
-    if (isset ($_GET["changeCat"])) {
-        if (!empty($_GET["checkList"])) {
-            foreach ($_GET['checkList'] as $selected) {
-                $catId = "$selected";
-                echo $catId;
-                ?>
-                <form method="get" action="categories.php">
-                    Ändra kategori: <input type="text" name="categoryChange">
-                    <input type="hidden" name="catId" value="<?php echo $catId ?>">
-                    <input type="submit" value="Ändra" name="changeCat2">
-                </form>
-                <?php
-            }
-        } 
-    }
-
-    if (isset ($_GET["changeCat2"])) {
-        if (!empty($_GET["categoryChange"])) {
-            $category = $_GET["categoryChange"];
+    // If-statement to check if button for changing categories in the category list 
+    if (isset ($_GET["changeCat2"])):
+        if (!empty($_GET["categoryChange"])):
+            $category = mysql_real_escape_string($_GET["categoryChange"]);
             $catId = $_GET["catId"];
             $query = "UPDATE categories SET category= '$category' WHERE id=$catId";
-            if ($stmt -> prepare($query)) {
+            if ($stmt -> prepare($query)):
                 $stmt->execute();
-            } else {
-                echo "fel";
-            }
-        }
-    }
+            else:
+                $errorMessage ="Faulty query in changeCat2";
+            endif;
+        else:
+            $errorMessage ="Du måste ange en ny kategori.";
+        endif;
+    endif;
 ?>
 
 <h1>Kategorier</h1>
 
-<?php
-
-// $query = "SELECT * FROM posts LEFT JOIN users ON posts.userid = users.id";
-$query = "SELECT * FROM categories";
-if ($stmt -> prepare($query)) {
-    $stmt-> execute();
-    $stmt -> bind_result($catId, $cat);
-?>
+    <!-- Form that prints all categories from the db with checkboxes -->
+    <!-- If change category is ordered an input field is printed -->
     <form method="get" action="categories.php">
-<?php
-    while (mysqli_stmt_fetch($stmt)) {
-        ?>
-        <input type="checkbox" name="checkList[]" value="<?php echo $catId ?>"> <?php echo $cat; ?>
-        <br>        
-        <?php
-    } // end while
+    <?php
+        while (mysqli_stmt_fetch($stmt)):
+    ?>
+            <input type="checkbox" name="checkList[]" value="<?php echo $catId; ?>"> <?php echo $cat; ?>
+            <?php
+                if ($catId == $changeCategoryId):
+            ?>
+                    <form method="get" action="categories.php">
+                        <input type="text" name="categoryChange">
+                        <input type="hidden" name="catId" value="<?php echo $catId; ?>">
+                        <input type="submit" value="Ändra" name="changeCat2">
+                    </form>
+            <?php
+                endif;
+            ?>
+           <br>    
+    <?php
+        endwhile;
     ?>
         <br>
         <input type="submit" value="Ta bort" name="removeCat">
@@ -86,8 +117,12 @@ if ($stmt -> prepare($query)) {
         Lägg till kategori: <input type="text" name="category">
         <input type="submit" value="Lägg till" name="addCat">
     </form>
-    <?php
-    
-}
+<?php
 
+    // Print error message 
+    if ($errorMessage != NULL):
+        echo $errorMessage;
+    endif;
+
+	include_once "../templates/footer.php"; // Footer.
 ?>
--- admin/users.php
+++ admin/users.php
@@ -1,87 +1,95 @@
 <?php
-    require_once (__DIR__."/../assets/db_connect.php");
+	include_once "../assets/db_connect.php"; // Database connection.
+    include_once "../templates/header.php"; // Header content.
 
-    if (isset ($_GET["addCat"])) {
-        $category = strip_tags($_GET["category"]);
-        $query = "INSERT INTO categories VALUES (NULL, '$category')";
-        if ($stmt -> prepare($query)) {
-            $stmt->execute();
-        } else {
-            echo "fel";
-        }
-    }
+    // Reset functions for the internal variables
+    $addUser = FALSE;
+    $errorMessage = NULL;
 
-    if (isset ($_GET["removeCat"])) {
-        if (!empty($_GET["checkList"])) {
-            foreach ($_GET['checkList'] as $selected) {
-                $catId = $selected;
-                $query = "DELETE FROM categories WHERE id=$catId";
-                if ($stmt -> prepare($query)) {
+    // Set key for printing register form
+    if (isset ($_GET["addUser"])):
+        $addUser = TRUE;
+    endif;
+
+    // If-statement to check if button for removing users is set 
+    // If button is pressed continue to check through the array  and
+    // for each category checked, remove it fromm the db
+    if (isset ($_GET["removeUser"])):
+        if (!empty($_GET["checkList"])):
+            foreach ($_GET['checkList'] as $selected):
+                $userId = $selected;
+                $query = "DELETE FROM users WHERE id=$userId";
+                if ($stmt -> prepare($query)):
                     $stmt->execute();
-                } else {
+                else:
                     echo "fel";
-                } //end if
-            } // end foreach              
-        } else {
+                endif;
+            endforeach;              
+        else:
             echo "fellist";
-        }
-    }
-            
+        endif;
+    endif;
 
-        if (isset ($_GET["changeCat"])) {
-            echo "button pressed";
-            if (!empty($_GET["checkList"])) {
-                echo "checked";
-                foreach ($_GET['checkList'] as $selected) {
-                    $catId = $selected;
-                    echo $selected;
-                    ?>
-                    <form method="get" action="categories.php">
-                        Ändra kategori: <input type="text" name="categoryChange">
-                        <input type="submit" value="Ändra" name="changeCat">
-                    </form>
-                    <?php
-                    $category = $_GET["categoryChange"];
-                    $query = "UPDATE categories SET category= $category WHERE id=$catId";
-                    if ($stmt -> prepare($query)) {
-                        $stmt->execute();
-                    } else {
-                        echo "fel";
-                    }
-                }
-            }
-        }
+    // Select all rows from the database users 
+    $query = "SELECT * FROM users";
+    if ($stmt -> prepare($query)):
+        $stmt-> execute();
+        $stmt -> bind_result($userId, $permission, $uName, $uPass, $uMail, $uWebSite, $ufName, $ulName, $uPic, $uDesc);
+    endif;
 ?>
 
-<h1>användare</h1>
+<h1>Användare</h1>
 
+    <!-- Form that prints all categories from the db with checkboxes -->
+    <!-- If change category is ordered an input field is printed -->
+    <form method="get" action="users.php">
 <?php
-
-// $query = "SELECT * FROM posts LEFT JOIN users ON posts.userid = users.id";
-$query = "SELECT * FROM users";
-if ($stmt -> prepare($query)) {
-    $stmt-> execute();
-    $stmt -> bind_result($userId, $permission, $uName, $uPass, $uMail, $uWebSite, $ufName, $ulName, $uPic, $uDesc);
-?>
-    <form method="get" action="categories.php">
-<?php
-    while (mysqli_stmt_fetch($stmt)) {
+    while (mysqli_stmt_fetch($stmt)):
         ?>
-        <input type="checkbox" name="checkList[]" value="<?php echo $catId ?>"> <?php echo $uName; ?>
+        <input type="checkbox" name="checkList[]" value="<?php echo $userId; ?>"> <?php echo $uName; ?>
         <br>        
-        <?php
-    } // end while
-    ?>
-        <br>
-        <input type="submit" value="Ta bort" name="removeUser">
-        <br>
-    </form>
-    <form method="get" action="categories.php">
-        Lägg till användare: <input type="text" name="category">
-        <input type="submit" value="Lägg till" name="addUser">
-    </form>
-    <?php
-    
-}
-
+<?php
+    endwhile;
 ?>
+        <br>
+        <input type="submit" value="Ta bort användare" name="removeUser">
+    </form>
+    <br>
+    <form method="get" action="users.php">
+        <input type="submit" value="Lägg till ny användare" name="addUser">
+    </form>
+    <br>
+    <?php
+        // if registration is ordered print registration form
+        if ($addUser == TRUE): 
+    ?>
+            <form method="POST" action="../assets/registercheck.php">
+
+                <fieldset>
+                    <legend>Lägg till ny användare</legend>
+                    <label>Användarnamn:</label> <br>
+                    <input type="text" name="userName"> <br>
+                    <label>Lösenord: </label> <br>
+                    <input type="password" name="passWord"> <br>
+                    <label>Förnamn:</label> <br>
+                    <input type="text" name="firstName"> <br>
+                    <label>Efternamn:</label> <br>
+                    <input type="text" name="lastName"> <br>
+                    <label>E-post:</label> <br>
+                    <input type="email" name="eMail"> <br>
+                    <label>Web-sida:</label> <br>
+                    <input type="text" name="webSite"> <br>
+                    <label>Beskrivning:</label> <br>
+                    <textarea cols="25" rows="7" name="description"></textarea> <br>
+                    <input id="button" type="submit" name="register" value="Lägg till">
+                </fieldset>
+            </form>
+    <?php
+        endif;
+
+        // Printing error message 
+        if ($errorMessage != NULL):
+            echo $errorMessage;
+        endif;
+        include_once "../templates/footer.php"; // Footer.
+    ?>
