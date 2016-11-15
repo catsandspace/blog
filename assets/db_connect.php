18a077782f2d9111e1df3a68b3ae0fa5c21dbfa7
xhkr.mac <hkrei@hodor.local>
2016-11-15 12:31:44 +0100

    db_connect conflict fix

--- assets/db_connect.php
+++ assets/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once __DIR__."/config.php";
+    require_once(__DIR__."/config.php");
 
     $conn = new mysqli($db_hostname, $db_user, $db_password, $db_name);
     $conn->set_charset("utf8");
@@ -8,11 +8,7 @@
     $databaseErrorMessage = "Det går inte att ansluta till databasen just nu (felkod: $databaseError).<br>Försök igen senare.";
 
     $stmt = $conn->stmt_init();
-<<<<<<< HEAD
-    
-=======
 
 
->>>>>>> master
 ?>
  
