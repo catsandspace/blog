b1381ae3ab4b5b93349213f32c6fe556109ebe81
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-15 16:55:47 +0100

    Changed indenting
    Edited:
    **header.php** <- Changed tab length from 2 to 4 spaces.

--- templates/header.php
+++ templates/header.php
@@ -6,8 +6,8 @@
    // Execute categories query.
    $query = "SELECT * FROM categories";
    if ($stmt->prepare($query)) {
-      $stmt->execute();
-      $stmt->bind_result($id, $category);
+        $stmt->execute();
+        $stmt->bind_result($id, $category);
    }
 
 ?>
@@ -15,22 +15,22 @@
 <!DOCTYPE html>
 <html lang="sv">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Cats and Space</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cats and Space</title>
 </head>
 <body>
-<header>
-   <img src="<?php echo $path; ?>img/logo.png" alt="logo">
-   <h1 class="logo-title">Cats and Space</h1>
-   <nav>
-      <ul>
-         <li><a href="<?php echo $path; ?>index.php">Hem</a></li>
-         <?php while (mysqli_stmt_fetch($stmt)): ?>
-         <li><a href="<?php echo $path; ?>index.php?display=<?php echo $id; ?>"><?php echo ucfirst($category); ?></a></li>
-         <?php endwhile?>
-         <li><a href="#">Kontakt</a></li>
-      </ul>
-   </nav>
-</header>
+    <header>
+       <img src="<?php echo $path; ?>img/logo.png" alt="logo">
+       <h1 class="logo-title">Cats and Space</h1>
+       <nav>
+            <ul>
+                <li><a href="<?php echo $path; ?>index.php">Hem</a></li>
+                <?php while (mysqli_stmt_fetch($stmt)): ?>
+                <li><a href="<?php echo $path; ?>index.php?display=<?php echo $id; ?>"><?php echo ucfirst($category); ?></a></li>
+                <?php endwhile?>
+                <li><a href="#">Kontakt</a></li>
+            </ul>
+       </nav>
+    </header>
