f208f60c755dd9ec28fe09472d15acf190f387af
xhkr-pc <hugo.kreipke@gmail.com>
2016-11-18 12:55:24 +0100

    Moved file_upload.php content to addpost.php
    To be able to link post.id with post.image column

--- admin/addpost.php
+++ admin/addpost.php
@@ -1,7 +1,7 @@
 <?php
     require_once "../templates/header.php";
     require_once "../assets/db_connect.php";
-    require_once "../assets/file_upload.php";
+    require_once "../assets/functions.php";
     require_once "../assets/session.php";
 
     // Redirect to login.php if no session active.
@@ -34,17 +34,34 @@
         // Escapes special characters in a string for use in an SQL statement
         if ($allRequiredFilled) {
             // TODO: Keep it dry. This needs som attention.
-            $image = mysqli_real_escape_string($conn, $targetName);
             $title = mysqli_real_escape_string($conn, $_POST["headline"]);
             $content = mysqli_real_escape_string($conn, $_POST["post-content"]);
             $published = mysqli_real_escape_string($conn, $_POST["publish"]);
             $category = mysqli_real_escape_string($conn, $_POST["category"]);
-            // $query = "UPDATE users SET profile_url = '{$target_name}' WHERE id = '{$_SESSION["userid"]}'";
-            $query = "INSERT INTO posts VALUES ('', 1, '', '', '{$image}', '{$title}', '{$content}', '{$published}', '{$category}')";
 
-            if ($stmt->prepare($query)) {
-        		$stmt->execute();
-                $feedbackMessage = "Inlägget laddades upp i databasen.";
+            $query = "INSERT INTO posts VALUES ('', 1, '', '', '', '{$title}', '{$content}', '{$published}', '{$category}')";
+            
+            // Statements for inserting and updating database values TODO: escape char.
+            if ($stmt->prepare($query)) { // 1st query -INSERTS values into db
+                $stmt->execute();
+                $imageId = $stmt->insert_id; // Catches the current post.id
+                $fileName = basename($_FILES["post-img"]["name"]); // The name of the file
+                $temporaryFile = $_FILES["post-img"]["tmp_name"]; // The temporary file and path
+                $type = pathinfo($fileName, PATHINFO_EXTENSION); // The file type
+                $fileError = checkUploadedFile($_FILES["post-img"]); // This checks if there are any file errors
+                $targetName = "../uploads/postimg/" . basename("postimg_") . $imageId . ".$type"; // Temporary $targetName for moving and renaming file
+
+                // Move uploaded file to "uploads/postimg/ and update $targetName to be appropiate path for table posts.image"
+                if (!$fileError) {
+                    move_uploaded_file($temporaryFile, $targetName);
+                    $targetName = "uploads/postimg/". basename("postimg_") . $imageId . ".$type";
+                    $updateQuery = "UPDATE posts SET image ='{$targetName}' WHERE id ='{$imageId}' ";
+                    $stmt->prepare($updateQuery);
+                    $stmt->execute();
+
+                    $feedbackMessage = "Inlägget laddades upp i databasen.";
+                }
+
             } else {
                 $feedbackMessage = "Du måste fylla i alla fält.";
             }
@@ -70,7 +87,7 @@
     ?>
     <input type="radio" name="publish" id="publish" value="1" required>
     <label for="publish">Publicera</label><br>
-    <input type="radio" name="publish" id="draft" value="0" required>
+    <input type="radio" name="publish" id="draft" value="2" required>
     <label for="draft">Spara som utkast</label><br>
     <label for="headline">Rubrik</label><br>
     <input type="text" name="headline" id="headline" placeholder="Rubrik" required><br>
@@ -81,7 +98,7 @@
 
         <?php while (mysqli_stmt_fetch($stmt)): ?>
         <input type="radio" name="category" value="<?php echo $id; ?>" required>
-        <label for="publish"><?php echo $category; ?></label><br>
+        <label for="publish"><?php echo ucfirst($category); ?></label><br>
         <?php endwhile; $stmt->close();?>
 
     </div>
@@ -89,6 +106,7 @@
 </form>
 <?php
     // This checks if there is a feedback message and prints it if true.
+    //TODO: NEEDS TO BE IN <p> or something, now loose in html.
     if (isset($_POST["submit"]) && $feedbackMessage) { echo $feedbackMessage; }
 ?>
 
