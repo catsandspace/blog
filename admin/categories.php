5c5a36b64db150bb0a13ad65135279c1389231b0
aalselius <alselius@telia.com>
2016-11-18 18:02:04 +0100

    categories and comments

--- admin/categories.php
+++ admin/categories.php
@@ -14,9 +14,9 @@
 
     // If-statement to check if button for adding new categories is set
     // If everything looks okay insert into db
-    if (isset ($_GET["addCat"])):
-        if (!empty($_GET["category"])): // Input given
-            $category = mysql_real_escape_string($_GET["category"]);
+    if (isset ($_POST["addCat"])):
+        if (!empty($_POST["category"])): // Input given
+            $category = mysql_real_escape_string($_POST["category"]);
             $query = "INSERT INTO categories VALUES (NULL, '$category')";
             if ($stmt->prepare($query)):
                 $stmt->execute();
@@ -31,9 +31,9 @@
     // If-statement to check if button for removing categories is set
     // If button is pressed continue to check through the array  and
     // for each category checked, remove it frpm the db
-    if (isset ($_GET["removeCat"])):
-        if (!empty($_GET["checkList"])):
-            foreach ($_GET['checkList'] as $selected):
+    if (isset ($_POST["removeCat"])):
+        if (!empty($_POST["checkList"])):
+            foreach ($_POST['checkList'] as $selected):
                 $catId = $selected;
                 $query = "DELETE FROM categories WHERE id=$catId";
                 if ($stmt->prepare($query)):
@@ -51,10 +51,10 @@
     // A counter is set to see if only one category is checked
     // The id for the checked category is memorized, if nothing is
     // checked the category id is set to NULL
-    if (isset ($_GET["changeCat"])):
-        if (!empty($_GET["checkList"])):
+    if (isset ($_POST["changeCat"])):
+        if (!empty($_POST["checkList"])):
             $count = 0;
-            foreach ($_GET['checkList'] as $selected):
+            foreach ($_POST['checkList'] as $selected):
                 $catId = $selected;
                 $count ++;
             endforeach;
@@ -68,10 +68,10 @@
     endif;
 
     // If-statement to check if button for changing categories in the category list
-    if (isset ($_GET["changeCat2"])):
-        if (!empty($_GET["categoryChange"])):
-            $category = mysql_real_escape_string($_GET["categoryChange"]);
-            $catId = $_GET["catId"];
+    if (isset ($_POST["changeCat"])):
+        if (!empty($_POST["categoryChange"])):
+            $category = mysql_real_escape_string($_POST["categoryChange"]);
+            $catId = $_POST["catId"];
             $query = "UPDATE categories SET name = '$category' WHERE id = $catId";
             if ($stmt -> prepare($query)):
                 $stmt->execute();
@@ -98,29 +98,36 @@
     FORM THAT PRINTS ALL CATEGORIES FROM DATABASE, INCLUDING CHECKBOXES
 *****************************************************************************-->
     <div class="flexboxWrapper">
-        <form method="get" action="categories.php" class="listWrapper">
+        <form method="post" action="categories.php" class="listWrapper">
             <div class="list">
-            <div class="inner-list">
-        <?php while (mysqli_stmt_fetch($stmt)): ?>
-                <input type="checkbox" name="checkList[]" value="<?php echo $catId; ?>"> <?php echo $cat; ?>
-                <?php if ($catId == $changeCategoryId): ?>
-                        <form method="get" action="categories.php">
-                            <input type="text" name="categoryChange">
-                            <input type="hidden" name="catId" value="<?php echo $catId; ?>">
-                            <button type="submit" value="Ändra" name="changeCat2" class="button">Ändra</button>
-                        </form>
-                <?php endif; ?>
-            <br>
-        <?php endwhile; ?>
-        </div>
+                <div class="inner-list">
+                <?php
+                    $change=FALSE;
+                    while (mysqli_stmt_fetch($stmt)): ?>
+                        <input type="checkbox" name="checkList[]" value="<?php echo $catId; ?>"> <?php echo $cat; ?>
+                        <?php
+                        if ($catId == $changeCategoryId):
+                                $change=TRUE;
+                                $changeCatId="$catId";
+                                $changeCat="$cat";
+                        endif; ?>
+                        <br>
+                <?php endwhile; ?>
+                </div>
             </div>
+            <br>
+            <?php if ($change): ?>
+                <label for="categoryChange">Ändra kategori <?php echo $changeCat; ?>:</label>
+                <input type="text" name="categoryChange">
+                <input type="hidden" name="catId" value='"$changeCat"'>
+            <?php endif; ?>
             <button type="submit" value="Ändra" name="changeCat" class="button">Ändra</button>
             <button type="submit" value="Ta bort" name="removeCat" class="button red">Ta bort</button>
-
         </form>
-        <form method="get" action="categories.php" class="inputWrapper">
+
+        <form method="post" action="categories.php" class="inputWrapper">
             <label for="addCatagory">Lägg till kategori:</label>
-            <input type="text" name="category" id="addCatagory">
+            <input type="text" name="category" id="addCategory">
             <button type="submit" value="Lägg till" name="addCat" class="button">Lägg till</button>
         </form>
     </div>
--- admin/comments.php
+++ admin/comments.php
@@ -1,21 +1,85 @@
 <?php
-     require_once "../templates/header.php"; // Header content.
+    require_once "../templates/header.php"; // Header content.
+    require_once "../assets/session.php";
+
+
+
+
+
+    // Redirect to login.php if no session active.
+    if (!isset($_SESSION["logged-in"]) && $_SESSION["logged-in"] == FALSE):
+        header("Location: ../login.php");
+    endif;
+
+    // If-statement to check if button for removing users is set
+    // If button is pressed continue to check through the array and
+    // for each category checked, remove it fromm the db
+    if (isset ($_GET["removeComment"])):
+        if (!empty($_GET["checkList"])):
+            foreach ($_GET['checkList'] as $selected):
+                $userId = $selected;
+                $query = "DELETE FROM comments WHERE id=$commentId";
+                if ($stmt -> prepare($query)):
+                    $stmt->execute();
+                else:
+                    echo "fel";
+                endif;
+            endforeach;
+        else:
+            echo "fellist";
+        endif;
+    endif;
+
+
+
+    // Select all rows from the database comments
+    $query = "SELECT * FROM comments";
+    if ($stmt -> prepare($query)):
+        $stmt-> execute();
+        $stmt -> bind_result($commentId, $userId, $date, $eMail, $name, $content, $postId);
+    endif;
+
+
 
 ?>
 
+<main>
+<h2>Användare</h2>
+    <form method="POST" action="./comments.php">
+        <table>
+            <thead>
+                <th>Ta bort</th>
+                <th>Kommentar</th>
+            </thead>
+            <tbody>
+                <?php while (mysqli_stmt_fetch($stmt)): ?>
+                <tr>
+                    <td><input type="checkbox" name="checkList[]" value="<?php echo $commentId; ?>"></td>
+                    <td><?php echo $content; ?></td>
+                </tr>
+                <tr>
+                    <td><?php echo $name; ?></td>
+                    <td><?php echo $eMail; ?></td>
+                    <td><?php echo $date; ?></td>
+                </tr>
+                <?php endwhile; ?>
+
+            </tbody>
+        </table>
+                        <button type="submit" value="Ta bort användare" name="removeComment"class="button red">Ta bort kommentarer</button>
+
+    </form>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php require_once "../templates/footer.php"; ?>
+    <br>
+    <?php
+        if (isset ($_GET["errorMessage"])):
+            if ($_GET["errorMessage"] != NULL):
+                echo $_GET["errorMessage"];
+            endif;
+        endif;
+    ?>
+    </main>
+<?php
+    require_once "../templates/footer.php";
+?>
