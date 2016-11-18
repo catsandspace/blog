fe6f9d7f87d01fc4bb95c47d5e8966a18c7e2d2f
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-18 13:53:49 +0100

    Added functionality so that deleted post is removed from database
    #Edited
    **admin/postlist.php** <- Added query that deletes the post in database.

--- admin/postlist.php
+++ admin/postlist.php
@@ -7,6 +7,31 @@
         header("Location: ../login.php");
     }
 
+/* -----------------------------------------------------------------------------
+   START OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
+----------------------------------------------------------------------------- */
+
+    $feedbackMessage = NULL;
+
+    if (isset($_POST["edit-post"])) {
+        $feedbackMessage = "Du vill redigera ett inlägg";
+    }
+
+    if (isset($_POST["delete-post"])) {
+
+        $postToDelete = $_POST["delete-post"];
+        $query = "DELETE FROM posts WHERE id ='{$postToDelete}'";
+
+        if ($stmt->prepare($query)) {
+            $stmt->execute();
+            $feedbackMessage = "Du har tagit bort inlägget";
+        }
+    }
+
+/* -----------------------------------------------------------------------------
+   END OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
+----------------------------------------------------------------------------- */
+
     // SQL statement with LEFT JOIN table -> posts & categories.
     // TODO: Just get the variables you need.
     $query  = "SELECT posts.*, categories.name FROM posts LEFT JOIN categories ON posts.categoryid = categories.id";
@@ -16,27 +41,8 @@
        $stmt->execute();
        $stmt->bind_result($id, $userId, $created, $updated, $image, $title, $content, $published, $categoryId, $categoryName);
    }
-
-/* -----------------------------------------------------------------------------
-   START OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
------------------------------------------------------------------------------ */
-
-    $feedbackMessage = "";
-
-    if (isset($_POST["edit-post"])) {
-        $feedbackMessage = "Du vill redigera ett inlägg";
-    }
-
-    if (isset($_POST["delete-post"])) {
-        $feedbackMessage = "Du vill ta bort ett inlägg";
-    }
-
-/* -----------------------------------------------------------------------------
-   END OF FEEDBACK MESSAGE THAT PRINTS INFO ABOUT THE DATABASE UPDATE
------------------------------------------------------------------------------ */
 ?>
 <h2>Inlägg</h2>
-<?php if ($feedbackMessage) { echo $feedbackMessage; } ?>
 <form method="POST" action="./postlist.php">
     <table>
         <thead>
@@ -51,10 +57,11 @@
                 <td><img src="../uploads/postimg/postlist-img.jpg" alt="Image of cats and space" class="postlist-img"></td>
                 <td><h3><?php echo $title; ?></h3></td>
                 <td><button type="submit" class="button" name="edit-post" value="<?php echo $id; ?>">Redigera</button></td>
-                <td><button type="submit" class="button" name="delete-post" value="<?php echo $id; ?>">Ta bort  </button></td>
+                <td><button type="submit" class="button" name="delete-post" value="<?php echo $id; ?>">Ta bort</button></td>
             </tr>
             <?php endwhile; ?>
         </tbody>
     </table>
 </form>
+<?php if ($feedbackMessage) { echo $feedbackMessage; } ?>
 <?php require_once "../templates/footer.php"; ?>
