34cba16aa9564a8b9e6b673dfe9ac8929492597c
Flandy85 <Gustavsson09@hotmail.com>
2016-11-19 15:42:20 +0100

    Added a <h3>headline and a <ul> list
    h3 headline: Kontaktinformation
    ul list containing names, telephone nummers and emal adresses.

--- contact.php
+++ contact.php
@@ -1,20 +1,32 @@
 <?php
     require_once "./templates/header.php"; // Header content.
-
-
-
  ?>
-
-
-
-
-
-
-
-
-
-
-
-
+ <main>
+    <div>
+        <h2>Kontaktinformation</h2>
+    </div>
+    <div>
+        <h3>Katarina</h3>
+        <ul>
+            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
+            <li class="mail"><a href="mailto:katarina.fantastic4@gmail.com">katarina.fantastic4@gmail.com</a></li>
+        </ul>
+        <h3>Alexandra</h3>
+        <ul>
+            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
+            <li class="mail"><a href="mailto:alexandra.fantastic4@gmail.com">alexandra.fantastic4@gmail.com</a></li>
+        </ul>
+        <h3>Hugo</h3>
+        <ul>
+            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
+            <li class="mail"><a href="mailto:hugo.fantastic4@gmail.com">hugo.fantastic4@gmail.com</a></li>
+        </ul>
+        <h3>Anders</h3>
+        <ul>
+            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
+            <li class="mail"><a href="mailto:anders.fantastic4@gmail.com">anders.fantastic4@gmail.com</a></li>
+        </ul>
+    </div>
+ </main>
 
  <?php require_once "./templates/footer.php"; ?> <!-- Footer content -->
