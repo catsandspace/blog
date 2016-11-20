2c0f485b6073549ab58ef08e94f967cb0e6fe9e4
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-20 13:45:41 +0100

    Removed non-working code
    # Edited:
    **index.php** <- Removed non-working code so that this branch can be
    merged.

--- index.php
+++ index.php
@@ -40,46 +40,4 @@
         </div>
     </article>
 <?php endwhile; ?>
-
-<?php
-/*******************************************************************************
-   START OF COMMENTS
-*******************************************************************************/
-
-    $totalNumberOfComments = NULL;
-    $errorMessage = NULL;
-
-    $stmtComments = $conn->stmt_init();
-    $query = "SELECT comments.* FROM comments LEFT JOIN posts ON comments.postid = posts.id";
-
-    if ($stmtComments->prepare($query)) {
-        $stmtComments->execute();
-        $stmtComments->bind_result($commentId, $userId, $commentCreated, $commentEmail, $commentAuthor, $commentContent, $postId);
-    }
-    else {
-        $errorMessage = "Något gick fel.";
-    }
-
-
-    while (mysqli_stmt_fetch($stmtComments)):
-    $stmtComments->store_result();
-    $numberOfComments = mysqli_stmt_num_rows($stmtComments);
-
-
-    // TODO: this if statement will be used once this is working properly.
-    // if ($id == $postId) {
-    echo "$commentContent<br>";
-    echo "Skriven av $commentAuthor<br><br>";
-    $totalNumberOfComments++;
-    // }
-
-    endwhile;
-    echo "Total number of comments: $totalNumberOfComments<br>";
-
-/*******************************************************************************
-   END OF COMMENTS
-*******************************************************************************/
-?>
-
-<?php if($errorMessage) { echo $errorMessage; }
-require_once "./templates/footer.php"; ?>
+<?php require_once "./templates/footer.php"; ?>
