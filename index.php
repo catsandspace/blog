bd0fd5665838f5db80f5099c487ea1287783dc51
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-20 14:24:07 +0100

    Fix: comments on index.php
    #Edited:
    **index.php** <- Changed the way posts are printed, now using an array.
    By doing this, $stmtComments = $conn->stmt_init() is now working, and
    comments are printed where they are supposed to be printed.

--- index.php
+++ index.php
@@ -23,21 +23,70 @@
         $stmt->execute();
         $stmt->bind_result($id, $userId, $created, $updated, $image, $title, $content, $published, $categoryId, $categoryName);
     }
-?>
-<?php while (mysqli_stmt_fetch($stmt)): ?>
+
+    $posts = array();
+    while (mysqli_stmt_fetch($stmt)) {
+        array_push($posts, array(
+            "id" => $id,
+            "image" => $image,
+            "title" => $title,
+            "content" => $content,
+            "categoryId" => $categoryId,
+            "categoryName" => $categoryName
+        ));
+    }
+
+    for ($i=0; $i < count($posts); $i++):
+        $post = $posts[$i];
+        ?>
+
     <article class="list">
         <div class="content-wrapper">
-            <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>">
+            <img src="<?php echo $post["image"]; ?>" alt="<?php echo $post["title"]; ?>">
             <div class="text-wrapper">
-                <h2><?php echo $title; ?></h2>
-                <p class="tag">Tags: <a href="?display=<?php echo $categoryId ?>"><?php echo str_replace(' ', '', $categoryName); ?></a> </p>
-                <p><?php echo $content; ?></p>
+                <h2><?php echo $post["title"]; ?></h2>
+                <p class="tag">Tags: <a href="?display=<?php echo $post["categoryId"] ?>"><?php echo str_replace(' ', '', $post["categoryName"]); ?></a> </p>
+                <p><?php echo $post["content"]; ?></p>
+
                 <div class="post-comments">
-                    <i class="fa fa-comment" aria-hidden="true"></i>
-                    <p>8</p>
-                </div>
+                    <div class="comment-wrapper">
+                        <?php // START OF COMMENTS
+
+                            $totalNumberOfComments = NULL;
+                            $errorMessage = NULL;
+
+                            $stmtComments = $conn->stmt_init();
+                            $query = "SELECT comments.* FROM comments LEFT JOIN posts ON comments.postid = posts.id";
+
+                            if ($stmtComments->prepare($query)) {
+                                $stmtComments->execute();
+                                $stmtComments->bind_result($commentId, $userId, $commentCreated, $commentEmail, $commentAuthor, $commentContent, $postId);
+                            }
+                            else {
+                                $errorMessage = "Något gick fel.";
+                            }
+
+                            while (mysqli_stmt_fetch($stmtComments)):
+                                $stmtComments->store_result();
+                                $numberOfComments = mysqli_stmt_num_rows($stmtComments);
+
+                                if ($post["id"] == $postId) {
+                                    echo "$commentContent<br>";
+                                    echo "Skriven av $commentAuthor<br><br>";
+                                    $totalNumberOfComments++;
+                            } endwhile;
+                        ?>
+                    </div>
+                <?php if ($totalNumberOfComments): ?>
+                        <i class="fa fa-comment" aria-hidden="true"></i>
+                        <p><?php echo "$totalNumberOfComments" ?></p>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </article>
-<?php endwhile; ?>
-<?php require_once "./templates/footer.php"; ?>
+<?php endfor; ?>
+
+
+<?php if($errorMessage) { echo $errorMessage; }
+require_once "./templates/footer.php"; ?>
