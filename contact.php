664da4a18309d66dd71357061821c97fc4f44f9a
Flandy85 <Gustavsson09@hotmail.com>
2016-11-21 12:30:32 +0100

    Added classes

--- contact.php
+++ contact.php
@@ -1,35 +1,35 @@
 <?php require_once "./templates/header.php"; // Header content. ?>
 <main>
-    <div>
-        <h2>Kontaktinformation</h2>
-    </div>
-    <div>
-        <h3>Katarina</h3>
-        <ul>
-            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
-            <li class="mail"><a href="mailto:katarina.fantastic4@gmail.com">katarina.fantastic4@gmail.com</a></li>
-        </ul>
-    </div>
-    <div>
-        <h3>Alexandra</h3>
-        <ul>
-            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
-            <li class="mail"><a href="mailto:alexandra.fantastic4@gmail.com">alexandra.fantastic4@gmail.com</a></li>
-        </ul>
-    </div>
-    <div>
-        <h3>Hugo</h3>
-        <ul>
-            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
-            <li class="mail"><a href="mailto:hugo.fantastic4@gmail.com">hugo.fantastic4@gmail.com</a></li>
-        </ul>
-    </div>
-    <div>
-        <h3>Anders</h3>
-        <ul>
-            <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
-            <li class="mail"><a href="mailto:anders.fantastic4@gmail.com">anders.fantastic4@gmail.com</a></li>
-        </ul>
+    <h2>Kontaktinformation</h2>
+    <div class="flexboxWrapper">
+        <div class="listWrapper">
+            <h3>Katarina</h3>
+            <ul>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
+                <li class="mail"><a href="mailto:katarina.fantastic4@gmail.com">katarina.fantastic4@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="listWrapper">
+            <h3>Alexandra</h3>
+            <ul>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="mail"><a href="mailto:alexandra.fantastic4@gmail.com">alexandra.fantastic4@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="listWrapper">
+            <h3>Hugo</h3>
+            <ul>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="mail"><a href="mailto:hugo.fantastic4@gmail.com">hugo.fantastic4@gmail.com</a></li>
+            </ul>
+        </div>
+        <div class="listWrapper">
+            <h3>Anders</h3>
+            <ul>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="mail"><a href="mailto:anders.fantastic4@gmail.com">anders.fantastic4@gmail.com</a></li>
+            </ul>
+        </div>
     </div>
 </main>
 
