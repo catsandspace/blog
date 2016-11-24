c192a57953f1e45b68bd770a5749389e7522cc51
Flandy85 <Gustavsson09@hotmail.com>
2016-11-24 09:23:50 +0100

    Discovered and removed 1 "/li" tag

--- contact.php
+++ contact.php
@@ -13,21 +13,21 @@
             <h3>Anders Gustavsson</h3>
             <ul>
                 <li class="mail"><a href="mailto:alexandra.fantastic4@gmail.com">Anders.fantastic4@catsandspace.com</a></li>
-                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
         <div class="flex-item">
             <h3>Hugo Kreipke</h3>
             <ul>
                 <li class="mail"><a href="mailto:hugo.fantastic4@gmail.com">hugo.fantastic4@catsandspace.com</a></li>
-                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
         <div class="flex-item">
             <h3>Katarina Ljungdahl</h3>
             <ul>
                 <li class="mail"><a href="mailto:anders.fantastic4@gmail.com">katarina.fantastic4@catsandspace.com</a></li>
-                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li></li>
+                <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
     </div>
