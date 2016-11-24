6e1314cab7a3e85fabcdead55cbad153c7a7e949
Flandy85 <Gustavsson09@hotmail.com>
2016-11-24 09:26:45 +0100

    Also changed email adresses in ( a tags mailto: ) to @catsandspace.com

--- contact.php
+++ contact.php
@@ -5,28 +5,28 @@
         <div class="flex-item">
             <h3>Alexandra Fagerström Alselius</h3>
             <ul>
-                <li class="mail"><a href="mailto:katarina.fantastic4@gmail.com">alexandra.fantastic4@catsandspace.com</a></li>
+                <li class="mail"><a href="mailto:katarina.fantastic4@catsandspace.com">alexandra.fantastic4@catsandspace.com</a></li>
                 <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
         <div class="flex-item">
             <h3>Anders Gustavsson</h3>
             <ul>
-                <li class="mail"><a href="mailto:alexandra.fantastic4@gmail.com">Anders.fantastic4@catsandspace.com</a></li>
+                <li class="mail"><a href="mailto:alexandra.fantastic4@catsandspace.com">Anders.fantastic4@catsandspace.com</a></li>
                 <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
         <div class="flex-item">
             <h3>Hugo Kreipke</h3>
             <ul>
-                <li class="mail"><a href="mailto:hugo.fantastic4@gmail.com">hugo.fantastic4@catsandspace.com</a></li>
+                <li class="mail"><a href="mailto:hugo.fantastic4@catsandspace.com">hugo.fantastic4@catsandspace.com</a></li>
                 <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
         <div class="flex-item">
             <h3>Katarina Ljungdahl</h3>
             <ul>
-                <li class="mail"><a href="mailto:anders.fantastic4@gmail.com">katarina.fantastic4@catsandspace.com</a></li>
+                <li class="mail"><a href="mailto:anders.fantastic4@catsandspace.com">katarina.fantastic4@catsandspace.com</a></li>
                 <li class="phone"><a href="tel:084429500">08-442 95 00</a></li>
             </ul>
         </div>
