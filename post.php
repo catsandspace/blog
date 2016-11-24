a288fe23d7b4d0572ebde2299cdb9930e460dbd4
xhkr-pc <hugo.kreipke@gmail.com>
2016-11-24 18:51:13 +0100

    Added <main> + checks if posts.updated has same value as posts.created

--- post.php
+++ post.php
@@ -8,6 +8,7 @@
     $post = array(
         "id" => "",
         "userid" => "",
+        "created" => "",
         "updated" => "",
         "image" => "",
         "title" => "",
@@ -27,7 +28,7 @@
 
         //$query  = "SELECT posts.*, categories.name FROM posts LEFT JOIN categories ON posts.categoryid = categories.id WHERE published = 1 AND id = '{$getPost}'";
 
-        // ^ Får inte $getPost att lira när jag kör LEFT JOIN WHHHYYYY!? ^
+        // ^ Problem with $getPost won't work with LEFT JOIN ??? ^
 
         $query = "SELECT * FROM posts WHERE id = '{$getPost}' AND published = 1";
 
@@ -35,10 +36,11 @@
             $stmt->execute();
             $stmt->bind_result($id, $userId, $created, $updated, $image, $title, $content, $published, $categoryId);
             $stmt->fetch();
-            $stmt->close();
+            //$stmt->close();
 
             $post["id"] = $id;
             $post["userid"] = $userId;
+            $post["created"] = $created;
             $post["updated"] = $updated;
             $post["image"] = $image;
             $post["title"] = $title;
@@ -46,7 +48,7 @@
             $post["categoryid"] = $categoryId;
             //$post["categoryname"] = $categoryName;
 
-            var_dump($post);
+            //var_dump($post);
 
             } else {
                 $errorMessage = "Något gick fel.";
@@ -55,11 +57,17 @@
     }
 
 /*******************************************************************************
+   GET COMMENTS ASSOCIATING WITH POST
+*******************************************************************************/
+
+
+/*******************************************************************************
    ERROR MESSAGE
 *******************************************************************************/
 
-    if ($id == NULL) {
-        $errorMessage = "Vi hittade inget inlägg med id: $getPost (visa 404-sidan?)";
+    if ($post["id"] == NULL) {
+        // TODO: Show 404-page instead?
+        $errorMessage = "Vi hittade inget inlägg med id: $getPost";
     }
 
 
@@ -67,19 +75,43 @@
    START OF HTML
 *******************************************************************************/
 ?>
-<?php if ($id != NULL): ?>
-<article class="list">
-        <div class="blogpost-wrapper">
+<main>
+
+<?php if ($post["id"] != NULL): ?>
+<!-- TODO: Make this semantic -->
+    <article class="">
+        <div class="post-test">
             <img src="<?php echo $post["image"]; ?>" alt="<?php echo $post["title"]; ?>">
-            <div class="blogpost-wrapper__text">
-                <h2><?php echo $post["title"]; ?></h2>
+            <div class="post-test__flex">
+                <p>Created: <?php echo $post["created"]; ?></p>
+
+                <?php if ($post["created"] != $post["updated"]): ?>
+                <p>Updated: <?php echo $post["updated"]; ?></p>
+                <?php endif; ?>
+                </div>
+
+                <p>Author: <?php echo $post["userid"]; ?></p>
                 <p class="tag">Tags: <a href="index.php?display=<?php echo $post["categoryid"] ?>"><?php echo str_replace(' ', '', $post["categoryid"]); ?></a> </p>
-                <p><?php echo $post["content"]; ?></p>
+                <h2>Title: <?php echo $post["title"]; ?></h2>
+                <p>Content: <?php echo $post["content"]; ?></p>
+            
+
+            <div class="post-test__comments">
+                <h3>Comments:</h3>
+
             </div>
-        </div>           
-</article>
 
-<form>
+            <div class="post-test__comments">
+                <h3>Add comment:</h3>
 
-</form>
+                <form>
+                    
+                </form>
+
+            </div>
+        </div>
+    </article>
+
+</main>
+
 <?php else: echo "<p class='error-msg'>".$errorMessage."</p>"; endif; ?>
