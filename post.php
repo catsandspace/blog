b6e7bb6a8ec0ac30e7d2766aa24552f305245032
Katarina Ljungdahl <jag@katarinaljungdahl.se>
2016-11-29 17:53:08 +0100

    Created new function to format inner html

--- post.php
+++ post.php
@@ -59,7 +59,9 @@
             $stmt->bind_result($id, $userId, $created, $updated, $image, $title, $content, $published, $categoryId, $categoryName, $postUsername);
             $stmt->fetch();
             //$stmt->close();
+            //
 
+            // TODO: If we need to define these, use them down below.
             $post["id"] = $id;
             $post["userid"] = $userId;
             $post["created"] = $created;
@@ -76,7 +78,14 @@
                 // TODO: 404?
                 $errorMessage = "Något gick fel.";
             }
+    }
 
+    function formatInnerHtml($string) {
+
+        $newString = str_replace('\n', "<br>", $string);
+        $newString = str_replace('\r', "", $newString);
+        $newString = str_replace('\\', "", $newString);
+        return $newString;
     }
 
 /*******************************************************************************
@@ -172,6 +181,7 @@
 *******************************************************************************/
 ?>
 <main>
+
 <?php if ($post["id"] != NULL): ?>
 <!-- TODO: Make this semantic -->
     <article class="smaller-font">
@@ -180,8 +190,8 @@
             <a class="relative-container__info" href="index.php?display=<?php echo $post["categoryid"] ?>"><?php echo str_replace(' ', '', $post["categoryname"]); ?></a>
         </div>
         <p class="saffron-text primary-brand-font">[Uppladdad av: <?php echo $post["username"]; ?>] [Publicerad: <?php echo $post["created"]; ?>] <?php if ($post["created"] != $post["updated"]): ?> [Uppdaterad: <?php echo $post["updated"]; ?>] <?php endif; ?></p>
-        <h2 class=""><?php echo $post["title"]; ?></h2>
-        <p><?php echo $post["content"]; ?></p>
+        <h2><?php echo $title; ?></h2>
+        <p><?php echo formatInnerHtml($content); ?></p>
         <?php if (!isset ($_POST["new-comment"])): ?>
         <form method="post" action="#">
             <button type="submit" name="new-comment" value="true" class="button margin-bottom-l">Kommentera inlägget</button>
