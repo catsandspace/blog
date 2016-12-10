8268a027f8685871d0459bff5892d6586a00b795
xhkr-pc <hugo.kreipke@gmail.com>
2016-12-10 14:20:25 +0100

    Added categories to desktop nav

--- templates/header.php
+++ templates/header.php
@@ -11,6 +11,13 @@
         $stmt->bind_result($id, $category);
    }
 
+   //TODO: Now when this variable is included in header we can remove it from dashbord etc.
+   if (isset($_SESSION["permission"])) {
+   $currentUsersPermission = $_SESSION["permission"];
+   } else {
+       $currentUsersPermission = NULL;
+   }
+
 ?>
 <!DOCTYPE html>
 <html lang="sv">
@@ -43,17 +50,33 @@
         </div>
         <nav class="nav-desktop">
             <ul class="nav-desktop__list">
-                <li class="nav-desktop__list-item">Start</li>
+                <a href="<?php echo $path; ?>index.php" class="nav-desktop__list-item"><li>Start</li></a>
                 <li class="nav-desktop__list-item">Kategorier
                     <ul class="nav-desktop__dropdown">
                         <?php while (mysqli_stmt_fetch($stmt)): ?>
-                        <li><a href="<?php echo $path; ?>index.php?display=<?php echo $id; ?>" class="nav-desktop__dropdown-item"><?php echo ucfirst($category); ?></a></li>
+                        <a href="<?php echo $path; ?>index.php?display=<?php echo $id; ?>" class="nav-desktop__dropdown-item"><li><?php echo ucfirst($category); ?></li></a>
                         <?php endwhile?>
                     </ul>
                 </li>
-                <li class="nav-desktop__list-item">Arkiv</li>
-                <li class="nav-desktop__list-item">Kontrollpanel</li>
-                <li class="nav-desktop__list-item">Kontakt</li>
+                <a href="<?php echo $path; ?>archive.php" class="nav-desktop__list-item"><li>Arkiv</li></a>
+
+                <?php if(isset($_SESSION["logged-in"]) && $_SESSION["logged-in"] == TRUE): ?>
+                <li class="nav-desktop__list-item">Kontrollpanel
+                    <ul class="nav-desktop__dropdown">
+                        <a href="<?php echo $path; ?>admin/posteditor.php" class="nav-desktop__dropdown-item"><li>Skapa nytt inlägg</li></a>
+                        <a href="<?php echo $path; ?>admin/postlist.php" class="nav-desktop__dropdown-item"><li>Se alla inlägg</li></a>
+                        <a href="<?php echo $path; ?>admin/comments.php" class="nav-desktop__dropdown-item"><li>Se alla kommentarer</li></a>
+                        <?php if ($currentUsersPermission == 1): ?>
+                        <a href="<?php echo $path; ?>admin/categories.php" class="nav-desktop__dropdown-item"><li>Hantera kategorier</li></a>
+                        <a href="<?php echo $path; ?>admin/users.php" class="nav-desktop__dropdown-item"><li>Hantera användare</li></a>
+                        <?php endif; ?>
+                        <a href="<?php echo $path; ?>admin/dashboard.php?statistics=true" class="nav-desktop__dropdown-item"><li>Se statistik</li></a>
+                        <a href="<?php echo $path; ?>assets/logout.php" class="nav-desktop__dropdown-item"><li>Logga ut</li></a>
+                    </ul>
+                </li>
+                <?php endif; ?>
+
+                <a href="<?php echo $path; ?>archive.php" class="nav-desktop__list-item"><li>Kontakt</li></a>
             </ul>
         </nav>
         <nav class="hamburger">
